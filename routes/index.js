673e51ff8de4f980686cf487075272ebb095b69f
dev-aceleradora-mobile <everis_amp@gmail.com>
2019-03-18 18:19:26 -0500

    scan files

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var upload = require('../Utils/utils').upload;
+var Utils = require('../Utils/utils');
+var upload = Utils.upload;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.post('submit', upload.single('file'), function (req, res, next) {
-  res.redirect('/');
+  if(!req.file) return res.redirect('/');
+
+  res.locals.files = Utils.getFilesUpload();
+
+  res.render('index');
+
 });
 module.exports = router;
